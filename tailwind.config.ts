1410da14d44a2b0546c900ab325701f63512ba91
tw20th <51616143+tw20th@users.noreply.github.com>
2025-01-20 12:08:55 +0900

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}", // App Router を対象に
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}", // 必要なら Pages Router 対応
   ],
   theme: {
     extend: {
